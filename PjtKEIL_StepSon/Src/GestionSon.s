7ac0df22b57f95575158a398973d2afe7d2cad0b
Aymeric Shinawi--Cros <shinawi-@insa-toulouse.fr>
2022-03-28 10:11:41 +0200

    Partie 1 de StepSon, on a récupéré les valeurs à envovyer dans SortieSon et ça marche

--- PjtKEIL_StepSon/Src/GestionSon.s
+++ PjtKEIL_StepSon/Src/GestionSon.s
@@ -1,17 +1,22 @@
 	PRESERVE8
 	THUMB   
 		
-
+	import LongueurSon
+	import Son
+	export CallbackSon
+	export decalageSon
+	export SortieSon
 ; ====================== zone de réservation de données,  ======================================
 ;Section RAM (read only) :
 	area    mesdata,data,readonly
-
+	
 
 ;Section RAM (read write):
 	area    maram,data,readwrite
 		
+SortieSon dcd 0
+decalageSon dcd 0
 
-	
 ; ===============================================================================================
 	
 
@@ -22,9 +27,65 @@
 ; écrire le code ici		
 
 
-
+CallbackSon proc
+		push {lr, r3, r4, r5, r6}
+		
+		ldr r1, =Son
+		ldr r2, =decalageSon
+		ldr r3, [r2]
+		ldr r4, =LongueurSon
+		ldr r5, [r4]
+		lsl r5, r5, #1
+		ldr R6, =SortieSon
+		
+		; ldr R4, =FlagCligno
+		; ldr R3, [R4]
+		; cmp R3, #0
+		
+		; si on est à la première itération...
+		cmp r3, #0
+		bne pasZero
+		
+		ldrsh r0, [r1]
+		mov r1, #720
+		add r0, #32768
+		mul r0, r0, r1
+		asr r0, #16
+		str r0, [r6]
+		
+		add r3, #2
+		str r3, [r2]
+		
+		
+		b endcallback
+		
+		
+pasZero
+		;si le son a fini d'etre lu, on sort
+		cmp r3, r5
+		bge endcallback
+		
+		;r0 contient l'échantillon
+		;r1 est l'adresse du son
+		;r2 est le décalage de l'adresse du son
+		ldrsh r0, [r1, r3]
+		mov r1, #720
+		add r0, #32768
+		mul r0, r0, r1
+		asr r0, #16
+		str r0, [r6]
+		
+		add r3, #2
+		str r3, [r2]
+				
+		b endcallback
 
 
 		
+endcallback
+		pop {r6, r5, r4, r3, lr}
+		;jouer le son contenu dans r0
+		bx lr
+		endp
 		
 	END	
--- PjtKEIL_StepSon/Src/principal.c
+++ PjtKEIL_StepSon/Src/principal.c
@@ -1,28 +1,31 @@
 
 
 #include "DriverJeuLaser.h"
-
-
+extern int PeriodeSonMicroSec;
+extern void CallbackSon(void);
 
 int main(void)
 {
 
-// ===========================================================================
-// ============= INIT PERIPH (faites qu'une seule fois)  =====================
-// ===========================================================================
+	// ===========================================================================
+	// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+	// ===========================================================================
 
-// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
-CLOCK_Configure();
+	// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+	CLOCK_Configure();
 
 
 	
-	
+	Timer_1234_Init_ff(TIM4, PeriodeSonMicroSec);
+	Active_IT_Debordement_Timer(TIM4, 2, CallbackSon);
+		
 
-//============================================================================	
+	//============================================================================	
+		
 	
-	
-while	(1)
+	while	(1)
 	{
+		
 	}
 }
 
